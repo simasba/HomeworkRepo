668e2e62d3af2c426d3d286854d43cc1c17bdd32
simasba <simas0002@gmail.com>
2023-01-22 17:43:31 +0000

    add src for .js file, add boxes with animations

--- main.js
+++ main.js
@@ -2,4 +2,18 @@ $(document).ready(function(){
     $("#interactive-button").on("click", function(){
         alert("Wow");
     });
+
+    var animations = ["animation1", "animation2", "animation3", "animation4", "animation5"];
+    $(".box").click(function(){
+      var currentAnimation = $(this).attr("class").split(" ")[1];
+      if (currentAnimation) {
+        $(this).removeClass(currentAnimation);
+      }
+      var randomAnimation = animations[Math.floor(Math.random() * animations.length)];
+      $(this).addClass(randomAnimation);
+      $(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+        $(this).removeClass(randomAnimation);
+      });
+    });
 });
+
