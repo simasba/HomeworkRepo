36d12121bbee6465a4d5f83ddc7573471a89ccc6
simasba <simas0002@gmail.com>
2023-02-10 00:07:42 +0000

    fix code formatting, update js code, change naming

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
-$(document).ready(function(){
-    $("#interactive-button").on("click", function(){
-        alert("Wow");
-    });
+$(document).ready(function () {
+  $("#interactive-button").on("click", function () {
+    alert("Wow");
+  });
 
-    var animations = ["animation1", "animation2", "animation3", "animation4", "animation5"];
-    $(".box").click(function(){
-      var currentAnimation = $(this).attr("class").split(" ")[1];
-      if (currentAnimation) {
-        $(this).removeClass(currentAnimation);
-      }
-      var randomAnimation = animations[Math.floor(Math.random() * animations.length)];
-      $(this).addClass(randomAnimation);
-      $(this).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
-        $(this).removeClass(randomAnimation);
-      });
-    });
+  var animations = [
+    "animation1",
+    "animation2",
+    "animation3",
+    "animation4",
+    "animation5",
+  ];
+  $(".box").click(function () {
+    var currentAnimation = $(this).attr("class").split(" ")[1];
+    if (currentAnimation) {
+      $(this).removeClass(currentAnimation);
+    }
+
+    $(this).addClass(getAnimationClass());
+  });
+
+  function getAnimationClass() {
+    return animations[Math.floor(Math.random() * animations.length)];
+  }
 });
-
